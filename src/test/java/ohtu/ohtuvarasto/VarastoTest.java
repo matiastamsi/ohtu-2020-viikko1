57e56f77220d850b0cd2eae886460b485cf33c86
Matias Tamsi <matias.tamsi@gmail.com>
2020-10-27 09:04:46 +0200

    Step 8 done (and all before it).

--- src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -64,5 +64,65 @@ public class VarastoTest {
         // varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         assertEquals(4, varasto.paljonkoMahtuu(), vertailuTarkkuus);
     }
+    
+    @Test
+    public void tilavuusMuutetaanNollaksiElleiPositiivinen() {
+        Varasto v = new Varasto(-2.0);
+        assertEquals(0.0, 0.0, v.getTilavuus());
+    }
+    
+    @Test
+    public void varastonLuominenKunSaldoJaTilavuusPositiivisia() {
+        Varasto v = new Varasto(123.321, 12.3);
+        assertEquals(123.321, 123.321, v.getTilavuus());
+        assertEquals(12.3, 12.3, v.getSaldo());
+    }
+    
+    @Test
+    public void varastonLuominenKunSaldoJaTilavuusNegatiivisia() {
+        Varasto v = new Varasto(-123.321, -12.3);
+        assertEquals(0.0, 0.0, v.getTilavuus());
+        assertEquals(0.0, 0.0, v.getSaldo());
+    }
+    
+    @Test
+    public void varastonLuominenKunLaikkyyYli() {
+        Varasto v = new Varasto(12, 123);
+        assertEquals(12, 12, v.getTilavuus());
+        assertEquals(12, 12, v.getSaldo());
+    }
+    
+    @Test
+    public void varastoMerkkijonona() {
+        Varasto v = new Varasto(3.0, 2.5);
+        assertEquals("saldo = 2.5, vielä tilaa 0.5", v.toString());
+    }
+    
+    @Test
+    public void negatiivinenLisaysEiTeeMitaan() {
+        Varasto v = new Varasto(1.1, 1.0);
+        v.lisaaVarastoon(-100);
+        assertEquals(1.0, 1.0, v.getSaldo());
+    }
+    
+    @Test
+    public void lisatessaYlimaarainenLaikkyyYli() {
+        Varasto v = new Varasto(5.0, 4.3);
+        v.lisaaVarastoon(1000);
+        assertEquals(5.0, 5.0, v.getSaldo());
+    }
+    
+    @Test
+    public void josVarastostaOtetaanNegatiivinenMaaraPalautaNolla() {
+        Varasto v = new Varasto(1.0, 0.9);
+        double m = v.otaVarastosta(-1);
+        assertEquals(0.0, 0.0, m);
+    }
+    
+    @Test
+    public void otetaanKaikkiMitaVoidaan() {
+        Varasto v = new Varasto(12.0, 10.0);
+        assertEquals(10.0, 10.0, v.otaVarastosta(11));
+    }
 
 }
